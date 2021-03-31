2b5baff6483c5dcf645642a5efd18e34486415b2
unknown <mkiricenko289@gmail.com>
2021-03-31 18:28:11 +0300

    Implement pin's ability to move. Create pin spawner object and implement ability to spawn pins

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    [SerializeField] float speed = 20f;
+    [SerializeField] Rigidbody2D rigidbody;
+    private bool isPinned = false;
+
+    void FixedUpdate()
+    {
+        if (isPinned == false)
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector2.up * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionObj)
+    {
+        if (collisionObj.tag == "Pin")
+        {
+            Debug.Log("Game OVER!!!");
+        }
+        else if (collisionObj.tag == "Rotator")
+        {
+            isPinned = true;
+            transform.SetParent(collisionObj.transform);
+        }
+    }
+}
